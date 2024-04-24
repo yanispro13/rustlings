8b358c42f1345489bee25d0aa15e7f6f1f3c1eb1
yanispro13 <yanissahkipro@gmail.com>
2024-04-24 17:02:34 +0200

    exo4

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,16 +1,7 @@
-// primitive_types3.rs
-//
-// Create an array with at least 100 elements in it where the ??? is.
-//
-// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
-// for a hint.
-
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a: i32: = 50 
 
-    if a.len() >= 100 {
+    if a >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
