a632a018806bcfeb9024e493333d20491ae5a28a
yanispro13 <yanissahkipro@gmail.com>
2024-04-24 14:35:27 +0200

    quiz et exo 3

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -1,21 +1,14 @@
-// if3.rs
-//
-// Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 pub fn animal_habitat(animal: &str) -> &'static str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        4
     };
 
-    // DO NOT CHANGE THIS STATEMENT BELOW
     let habitat = if identifier == 1 {
         "Beach"
     } else if identifier == 2 {
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -1,25 +1,10 @@
-// quiz1.rs
-//
-// This is a quiz for the following sections:
-// - Variables
-// - Functions
-// - If
-//
-// Mary is buying apples. The price of an apple is calculated as follows:
-// - An apple costs 2 rustbucks.
-// - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
-// Write a function that calculates the price of an order of apples given the
-// quantity bought.
-//
-// No hints this time ;)
-
-// I AM NOT DONE
-
-// Put your function here!
-// fn calculate_price_of_apples {
-
-// Don't modify this function!
-#[test]
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    if quantity <= 40 {
+        quantity * 2
+    } else {
+        quantity
+    }
+}
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
     let price2 = calculate_price_of_apples(40);
